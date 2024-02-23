3a784b8f4ff0995e210c6b4c9e36913fd25a3bc8
EduardoBan <banueloscarrilloe@gmail.com>
2024-02-23 16:42:19 -0600

    first commit

---
+++ build/keysuser.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'monorail.proxy.rlwy.net',
+        user: 'root',
+        password: 'f1dHHfHdehb5FFD12dbcbhFf2ChG-F5E',
+        database: 'crepas_users_db',
+        timezone: '+00:00',
+        port: 21768
+    }
+};
